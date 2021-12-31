37326f16847b2269e2cb61292050f44932eb5ad4
fjplaurr <fjplaurr@gmail.com>
2021-12-31 13:36:11 +0100

    chore: add security middlewares

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ import express from 'express'
 import { Low, JSONFile } from 'lowdb'
 import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
+import rateLimit from 'express-rate-limit'
+import helmet from 'helmet'
 
 const app = express()
-app.use(express.json())
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const file = join(__dirname, 'db.json')
 const adapter = new JSONFile(file)
 const db = new Low(adapter)
+
+// JSON middleware
+app.use(express.json({ limit: '10kb' }));
+
+// Limiter middleware
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100, 
+  message: 'Too many requests'
+})
+app.use(limiter)
+
+// Helmet middleware
+app.use(helmet());
 
 let users;
 
